d25b63a9c5faf7b3db0b2bdafd3153feb308cf8b
Yan <diesel92@inbox.ru>
2019-01-29 14:22:42 +0200

    prod tested w/o Jenkins. renamed files, updated build_pipe

--- jenkins/jobs/deploy_pipe_lucy.groovy
+++ jenkins/jobs/deploy_pipe_lucy.groovy
@@ -12,7 +12,7 @@ node ('master') {
 	    def buildNum = Jenkins.instance.getItem("${BUILD_JOB}").lastSuccessfulBuild.number
 	    ws("$JENKINS_HOME/workspace/${BUILD_JOB}") {
 	    sh "kubectl set image -f kubernetes/deployment_k8s_lucy.yaml lucy-app-ctr=bzumby/lucy_app:v1.${buildNum}"
-		sh 'pulseaudio -D'
+		// sh 'pulseaudio -D' // not sure yet.
 	}
 
 }
